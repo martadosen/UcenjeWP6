8836aa233ef07e18d5e3d033db9cee5d1e6663b7
Marta Došen <martagranat18@gmail.com>
2024-11-13 18:25:10 +0100

    Zoo

--- zoo.sql
+++ zoo.sql
@@ -12,32 +12,35 @@ go
 use zoo;
 go
 
---create table zivotinja(
---sifra int,
---vrsta varchar(50),
---ime varchar(50),
---djelatnik int,
---prostorija int,
---datum int
---);
 
-create table prostorija(
-sifra int,
-dimenzije varchar(30),
-maksimalanbroj int,
-mjesto varchar(30)
-);
 
 create table djelatnik(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
 iban varchar(50)
 );
 
 create table datum(
-datumrodenja datetime,
-datumdolaska datetime
-datumsmrti datetime
-sif
+datumrodenja datetime not null,
+datumdolaska datetime not null,
+datumsmrti datetime,
+sifra int not null primary key identity(1,1) 
+);
+
+
+create table prostorija(
+sifra int not null primary key identity(1,1),
+dimenzije varchar(30) not null,
+maksimalanbroj int,
+mjesto varchar(30)
+);
+
+create table zivotinja(
+sifra int not null primary key,
+vrsta varchar(50) not null,
+ime varchar(50) not null,
+djelatnik int not null references djelatnik(sifra),
+prostorija int not null references prostorija(sifra),
+datum int not null references datum(sifra)
 );
