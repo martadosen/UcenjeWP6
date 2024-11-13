8b3f074b93c2cf285fd16bd9fa7b0fa28dd83fd6
Marta Došen <martagranat18@gmail.com>
2024-11-13 17:59:27 +0100

    Promjene

--- zoo.sql
+++ zoo.sql
@@ -1,7 +1,16 @@
-
---create database zoo
-
---use zoo
+-- Ovo je SQL skripta
+-- prebacujem se na master bazu
+use master;
+go 
+-- brišem postojeæu bazu
+drop database if exists zoo;
+go
+-- kreiram novu bazu
+create database zoo;
+go
+-- pozicioniram se na novu bazu
+use zoo;
+go
 
 --create table zivotinja(
 --sifra int,
@@ -11,3 +20,24 @@
 --prostorija int,
 --datum int
 --);
+
+create table prostorija(
+sifra int,
+dimenzije varchar(30),
+maksimalanbroj int,
+mjesto varchar(30)
+);
+
+create table djelatnik(
+sifra int,
+ime varchar(50),
+prezime varchar(50),
+iban varchar(50)
+);
+
+create table datum(
+datumrodenja datetime,
+datumdolaska datetime
+datumsmrti datetime
+sif
+);
