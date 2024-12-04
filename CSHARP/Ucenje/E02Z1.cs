98209fb8c91015abf96e1d87f517689844b53a7a
Marta Došen <martagranat18@gmail.com>
2024-12-04 18:08:26 +0100

    E02Z1

---
+++ CSHARP/Ucenje/E02Z1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E02Z1
+    {
+
+        public static void Izvedi()
+        {
+            Console.Write("Upiši ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Upiši broj godina (cijeli broj): ");
+            int godine = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Osoba {0} ima {1} godine", ime, godine);
+            
+
+
+
+
+
+
+
+        }
+
+        
+
+
+
+
+
+
+    }
+}
